86b0e6dbd76e631fe8d898f404668a7c1b2d0c10
no-glue <nikola_pav@eunet.rs>
2015-01-20 14:23:17 +0100

    file size

--- file_size.py
+++ file_size.py
@@ -1,10 +1,22 @@
 #! /usr/bin/env python
 import sys
 import os
+import argparse
+
+def size(format, file):
+    statInfo = os.stat(file)
+    if format == "b":
+        return statInfo.st_size
+    elif format == "mb":
+        return statInfo.st_size / 1024 / 1024
+    elif format == "gb":
+        return statInfo.st_size / 1024 / 1024 / 1024
+    return 0
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        quit()
-    fileName = sys.argv[1]
-    statInfo = os.stat(fileName)
-    print statInfo.st_size
+    argv = sys.argv[1:]
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-format')
+    parser.add_argument('-file')
+    args = parser.parse_args(argv)
+    print size(args.format, args.file)
